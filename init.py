001c037ef75640c5d5fb1f9dba1c6fcece1f07cd
hanna-bae <bhn4542@gmail.com>
2024-05-06 19:21:51 +0900

    init

---
+++ init.py
@@ -0,0 +1,12 @@
+# turn on the terminal and install the streamlit
+# pip install streamlit 
+
+# import the streamlit
+import streamlit as st 
+
+def main():
+    st.title('Hello world!')
+
+
+if __name__ == '__main__':
+    main()
